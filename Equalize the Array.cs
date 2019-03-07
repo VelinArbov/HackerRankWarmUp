539ca0062d7a46b8ac914342d2128f43d338d9fe
VelinArbov <48315855+VelinArbov@users.noreply.github.com>
2019-03-07 15:09:10 +0200

    Add files via upload

---
+++ Equalize the Array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equalize_the_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int numbers = int.Parse(Console.ReadLine());
+
+            List<int> elements = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int commons = elements.GroupBy(c => c).Max(group => group.Count());
+            Console.WriteLine(elements.Count-commons);
+            }
+        }
+    }
+
