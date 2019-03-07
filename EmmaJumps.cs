575d84c7ef75a03126a040ba11ec1ff6fe24323b
VelinArbov <48315855+VelinArbov@users.noreply.github.com>
2019-03-07 14:10:29 +0200

    Add files via upload

---
+++ EmmaJumps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmmaJump
+{
+    class EmmaJumps
+    {
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+            List<int> clouds = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int count = 0;
+            int i = 0;
+            while (i < clouds.Count - 1)
+            {
+                if (i + 2 < clouds.Count && clouds[i + 2] != 1)
+                {
+                    count++;
+                    i = i + 2;
+                }
+                else
+                {
+                    count++;
+                    i++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
