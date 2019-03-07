ef6907e1d40021a0d9a9ee2d37ecd84c8546f9e5
VelinArbov <48315855+VelinArbov@users.noreply.github.com>
2019-03-07 13:21:30 +0200

    Add files via upload

---
+++ Counting Valleys.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hackerrankwarmUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string valey = Console.ReadLine();
+            int level = 0;
+            int valeys = 0;
+            for (int i = 0; i < number; i++)
+            {
+                char currentSymbol = valey[i];
+
+                if(currentSymbol == 'D')
+                {
+                    level--;
+                }
+                else
+                {
+                    level++;
+                }
+                if ( level== 0 && currentSymbol =='U')
+                {
+                    valeys++;
+                }
+
+            }
+           
+            Console.WriteLine(valeys);
+
+
+        }
+    }
+}
